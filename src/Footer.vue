6e1574df590b844dd6095c5e4184934e616a518b
iRuxu <rx6@qq.com>
2020-05-23 06:54:31 +0800

    update

--- src/Footer.vue
+++ src/Footer.vue
@@ -13,9 +13,9 @@
         <div class="c-footer-right">
             <a class="u-about" :href="about" target="_blank">关于我们</a>
             <em>|</em>
-            <a class="u-doc" href="https://github.com/JX3BOX/apidocs/wiki" target="_blank">帮助文档</a>
+            <a class="u-doc" href="https://www.jx3box.com/help" target="_blank">帮助文档</a>
             <em>|</em>
-            <a class="u-log" href="https://github.com/JX3BOX/apidocs/issues" target="_blank">更新日志</a>
+            <a class="u-log" href="https://www.jx3box.com/system" target="_blank">更新日志</a>
             <em>|</em>
             <a class="u-feedback" :href="feedback" target="_blank">反馈建议</a>
         </div>
