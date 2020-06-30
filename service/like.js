af9999a834712b8f3c745dac06d78507d2521eb5
iRuxu <rx6@qq.com>
2020-07-01 01:23:30 +0800

    新版统计

--- service/like.js
+++ service/like.js
@@ -1,8 +1,8 @@
 import { $,axios } from "./axios";
 import {__next} from '@jx3box/jx3box-common/js/jx3box.json'
 
-// const API = __next + 'api/post/'
-const API = '/api/post/'
+const API = __next + 'api/post/'
+// const API = '/api/post/'
 
 function addLike(id,type) {
     // return $.post("post/like",{
