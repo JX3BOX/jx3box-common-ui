f17b01f2afdcbbf4691dd8ef239dec83eb897726
iRuxu <rx6@qq.com>
2020-07-11 19:08:57 +0800

    fix types' ”

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 Vue.use(ElementUI);
 import "@jx3box/jx3box-common/css/element.css";
+import "@jx3box/jx3box-common/css/normalize.css";
 
 import App from './App.vue'
 
