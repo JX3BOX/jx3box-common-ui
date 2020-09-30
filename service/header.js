e79e88147323783c6527708cf76dd121fc4e96e1
iRuxu <rx6@qq.com>
2020-09-30 17:12:37 +0800

    fix

--- service/header.js
+++ service/header.js
@@ -1,11 +1,10 @@
 import { axios, $ } from "./axios";
 import { __helperUrl } from "@jx3box/jx3box-common/js/jx3box.json";
 
-function getMsg(uid) {
-    // let condition = encodeURIComponent("where[user_id]");
-    return axios.get(
-        __helperUrl + "api/messages/unread_total"
-    );
+function getMsg() {
+    return $.get(__helperUrl + "api/messages/unread_total",{
+        withCredentials: true,
+    });
 }
 
 function doLogout() {
