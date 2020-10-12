ca514c0b74dbd719bafb258a2f2fabdf2039c009
iRuxu <rx6@qq.com>
2020-10-12 14:51:37 +0800

    fix

--- src/Header.vue
+++ src/Header.vue
@@ -135,9 +135,9 @@
                                     <em>(UID : {{ user.uid }})</em></a
                                 >
                             </li>
-                            <li>
+                            <!-- <li>
                                 <a class="u-vip" href="/vip"><i class="u-icon-vip"></i> 会员中心</a>
-                            </li>
+                            </li> -->
                             <hr />
                             <li>
                                 <a href="/dashboard/#/work">作品管理</a>
