e7cd657f0dfc2f537c233d990fb74be6ae317b30
iRuxu <rx6@qq.com>
2021-03-31 13:00:08 +0800

    头像

--- src/Author.vue
+++ src/Author.vue
@@ -1,38 +1,24 @@
 <template>
     <div class="c-author" v-if="data">
         <div class="u-author">
-            <a :href="link" class="u-avatar">
-                <img
-                    class="u-avatar-pic"
-                    :src="data.avatar | avatar"
-                    :alt="data.name"
-                    :class="{ isCircle }"
-                />
-                <i class="u-avatar-frame" v-if="frameName"
-                    ><img :src="frameUrl"
-                /></i>
-            </a>
-            <a class="u-name" :href="link">
+            <Avatar
+                class="u-avatar"
+                :id="id"
+                :url="data.avatar"
+                :size="68"
+                :frame="data.avatar_frame"
+            />
+            <a class="u-name" :href="id | authorLink">
                 <span>{{ data.name }}</span>
             </a>
         </div>
         <div class="u-bio">{{ data.bio }}</div>
         <div class="u-link" v-if="hasLink">
-            <a
-                v-if="data.weibo_name"
-                class="u-weibo"
-                :href="data.weibo_url"
-                target="_blank"
-            >
+            <a v-if="data.weibo_name" class="u-weibo" :href="data.weibo_url" target="_blank">
                 <img svg-inline src="../assets/img/author/weibo.svg" />
                 {{ data.weibo_name }}
             </a>
-            <a
-                v-if="data.github_name"
-                class="u-github"
-                :href="data.github_url"
-                target="_blank"
-            >
+            <a v-if="data.github_name" class="u-github" :href="data.github_url" target="_blank">
                 <img svg-inline src="../assets/img/author/github.svg" />
                 {{ data.github_name }}
             </a>
@@ -40,16 +26,9 @@
                 <img src="../assets/img/author/tuilan.png" />
                 {{ data.tuilan_id }}
             </div>
-            <a
-                v-if="data.tv_type && data.tv_id"
-                class="u-tv"
-                :href="tv_link"
-                target="_blank"
-            >
+            <a v-if="data.tv_type && data.tv_id" class="u-tv" :href="tv_link" target="_blank">
                 <img :src="tv_img" />
-                <span class="u-tv-num">
-                    {{ data.tv_id }}
-                </span>
+                {{ data.tv_id }}
                 <span class="u-tv-living" v-if="tv_status">
                     <div class="u-tv-living-icon">
                         <div class="u-tv-living-icon-col first"></div>
@@ -66,10 +45,7 @@
             </div>
             <div class="u-medals" v-if="medals && medals.length">
                 <span class="u-medal" v-for="(item, i) in medals" :key="i">
-                    <img
-                        :src="item.medal | showTeamMedal"
-                        :title="medal_map[item.medal]"
-                    />
+                    <img :src="item.medal | showTeamMedal" :title="medal_map[item.medal]" />
                 </span>
             </div>
         </div>
@@ -79,51 +55,42 @@
 
 <script>
 const liveStatusMap = ["等待开播", "直播中", "直播结束"];
-import { showAvatar, authorLink, tvLink } from "@jx3box/jx3box-common/js/utils";
+import Avatar from "./Avatar.vue";
+import { authorLink, tvLink } from "@jx3box/jx3box-common/js/utils";
 import { __server, __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
-import {
-    getUserOverview,
-    getDouyu,
-    getUserMedals,
-    getFrames,
-} from "../service/author";
+import { getUserOverview, getDouyu, getUserMedals } from "../service/author";
 import { user as medal_map } from "@jx3box/jx3box-common/data/medals.json";
-import frames from "@jx3box/jx3box-common/data/user_avatar_frame.json";
 export default {
     name: "Author",
     props: ["uid"],
-    data: function() {
+    data: function () {
         return {
             data: "",
             tv: "",
             medals: [],
             medal_map,
-            frames,
         };
     },
     computed: {
-        id: function() {
+        id: function () {
             return this.uid;
         },
-        link: function() {
-            return authorLink(this.id);
-        },
-        tv_type: function() {
+        tv_type: function () {
             return this.data && this.data.tv_type;
         },
-        tv_id: function() {
+        tv_id: function () {
             return (this.data && this.data.tv_id) || 0;
         },
-        tv_img: function() {
+        tv_img: function () {
             return __imgPath + "image/tv/" + this.tv_type + ".png";
         },
-        tv_link: function() {
+        tv_link: function () {
             return tvLink(this.tv_type, this.tv_id) || "";
         },
-        tv_status: function() {
+        tv_status: function () {
             return (this.tv && this.tv.show_status == 1) || false;
         },
-        hasLink: function() {
+        hasLink: function () {
             return (
                 this.data.weibo_name ||
                 this.data.github_name ||
@@ -131,42 +98,23 @@ export default {
                 this.data.tv_id
             );
         },
-        hasTrophy: function() {
+        hasTrophy: function () {
             return this.medals.length;
-        },
-        frameName: function() {
-            return this.data.avatar_frame && this.frames[this.data.avatar_frame]
-                ? this.data.avatar_frame
-                : "";
-        },
-        frameUrl: function() {
-            if (this.frameName) {
-                let fileName = this.frames[this.frameName].files.s.file;
-                return __imgPath + `image/avatar/${this.frameName}/${fileName}`;
-            }
-            return "";
-        },
-        isCircle: function() {
-            return (
-                this.frameName && this.frames[this.frameName].style == "circle"
-            );
         },
     },
     filters: {
-        avatar: function(val) {
-            return showAvatar(val);
-        },
-        showTeamMedal: function(val) {
+        showTeamMedal: function (val) {
             return __imgPath + "image/medals/team/" + val + "-20.gif";
         },
+        authorLink,
     },
     methods: {
-        loadData: function() {
+        loadData: function () {
             return getUserOverview(this.id).then((res) => {
                 this.data = res.data.data;
             });
         },
-        loadTV: function() {
+        loadTV: function () {
             if (this.tv_type == "douyu") {
                 if (!this.tv_id || isNaN(this.tv_id)) return;
                 getDouyu(this.tv_id).then((res) => {
@@ -174,31 +122,26 @@ export default {
                 });
             }
         },
-        loadMedals: function() {
+        loadMedals: function () {
             if (!this.id) return;
             getUserMedals(this.id).then((res) => {
                 this.medals = res.data.data;
             });
         },
-        loadFrames: function() {
-            getFrames().then((res) => {
-                if (res.data) {
-                    this.frames = res.data;
-                }
-            });
-        },
     },
     watch: {
-        uid: function() {
+        uid: function () {
             this.loadData();
         },
     },
-    mounted: function() {
+    mounted: function () {
         this.loadData().then(() => {
             // this.loadTV();
             this.loadMedals();
-            this.loadFrames();
         });
+    },
+    components: {
+        Avatar,
     },
 };
 </script>
---
+++ src/Avatar.vue
@@ -0,0 +1,130 @@
+<template>
+    <a class="c-avatar" :href="uid | authorLink">
+        <img :src="showAvatar(avatar)" class="c-avatar-pic" :class="{ isCircle }" />
+        <i class="c-avatar-frame" v-if="frameName" :class="style">
+            <img :src="frameUrl" />
+        </i>
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+import frames from "@jx3box/jx3box-common/data/user_avatar_frame.json";
+import { __server, __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import { getFrames } from "../service/author";
+import { showAvatar, authorLink } from "@jx3box/jx3box-common/js/utils";
+export default {
+    name: "",
+    props: ["id", "url", "size", "frame"],
+    components: {},
+    data: function () {
+        return {
+            frames,
+            style_map: [60, 84, 136, 224],
+            style_cls: ["xs", "s", "m", "l"],
+        };
+    },
+    computed: {
+        uid: function () {
+            return ~~this.id || 0;
+        },
+        avatar: function () {
+            return this.url;
+        },
+        frameName: function () {
+            return this.frame && this.frames[this.frame] ? this.frame : "";
+        },
+        frameUrl: function () {
+            if (this.frameName) {
+                let fileName = this.frames[this.frameName].files[this.style]
+                    .file;
+                return __imgPath + `image/avatar/${this.frameName}/${fileName}`;
+            }
+            return "";
+        },
+        isCircle: function () {
+            return (
+                this.frameName && this.frames[this.frameName].style == "circle"
+            );
+        },
+        style: function () {
+            let style = ~~this.size;
+            this.style_map.forEach((item,i) => {
+                if(style < item){
+                    return this.style_cls[i]
+                }
+            })
+            return 's'
+        },
+    },
+    methods: {
+        loadFrames: function () {
+            getFrames().then((res) => {
+                if (res.data) {
+                    this.frames = res.data;
+                }
+            });
+        },
+        showAvatar: function (val) {
+            return showAvatar(val, this.size);
+        },
+    },
+    filters: {
+        authorLink,
+    },
+    created: function () {
+        this.loadFrames();
+    },
+    mounted: function () {},
+};
+</script>
+
+<style scoped lang="less">
+.c-avatar {
+    .pr;
+    .dbi;
+}
+.c-avatar-pic {
+    .db;
+    .full;
+    .r(4px);
+    &.isCircle {
+        .r(100%);
+    }
+}
+.c-avatar-frame {
+    .pa;
+    img {
+        .db;
+        .pa;
+        .lt(0);
+        .size(100%);
+    }
+
+    &.xs {
+        left: -6px;
+        top: -6px;
+        width: 60px;
+        height: 60px;
+    }
+
+    &.s {
+        left: -8px;
+        top: -8px;
+        width: 84px;
+        height: 84px;
+    }
+    &.m {
+        left: -8px;
+        top: -8px;
+        width: 136px;
+        height: 136px;
+    }
+    &.l {
+        width: 224px;
+        height: 224px;
+        left: -22px;
+        top: -22px;
+    }
+}
+</style>
