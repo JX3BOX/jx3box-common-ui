784a5bf27983488c0b1436966e511dba356e1552
iRuxu <rx6@qq.com>
2021-04-03 14:45:48 +0800

    fix header menu

--- src/header/nav.vue
+++ src/header/nav.vue
@@ -2,39 +2,36 @@
     <nav class="c-header-nav" v-if="nav">
         <div class="u-item-box" v-for="item in nav" :key="item.key">
             <template v-if="item.status && item.children && item.children.length">
-                <template v-if="item.status">
-                    <el-dropdown
-                        class="u-menu"
-                        :show-timeout="0"
-                        trigger="hover"
+                <el-dropdown
+                    class="u-menu"
+                    :show-timeout="0"
+                    trigger="hover"
+                >
+                    <a
+                        class="u-item el-dropdown-link"
+                        :class="{ on: isFocus(item.link) }"
+                        :href="item.link"
+                        >{{ item.label
+                        }}<i class="el-icon-arrow-down el-icon--right"></i
+                    ></a>
+                    <el-dropdown-menu
+                        slot="dropdown"
+                        class="c-header-menu"
                     >
-                        <a
-                            class="u-item el-dropdown-link"
-                            :class="{ on: isFocus(item.link) }"
-                            :href="item.link"
-                            >{{ item.label
-                            }}<i class="el-icon-arrow-down el-icon--right"></i
-                        ></a>
-                        <el-dropdown-menu
-                            slot="dropdown"
-                            class="c-header-menu"
-                            v-if="item.hasChildren"
+                        <el-dropdown-item
+                            v-for="subitem in item.children"
+                            :key="subitem.key"
+                            class="u-menu-item"
                         >
-                            <el-dropdown-item
-                                v-for="subitem in item.children"
-                                :key="subitem.key"
-                                class="u-menu-item"
-                            >
-                                <a :href="subitem.link" v-if="subitem.status"
-                                    >{{ subitem.label }}
-                                    <span v-if="subitem.desc">{{
-                                        subitem.desc
-                                    }}</span></a
-                                ></el-dropdown-item
-                            >
-                        </el-dropdown-menu>
-                    </el-dropdown>
-                </template>
+                            <a :href="subitem.link" v-if="subitem.status"
+                                >{{ subitem.label }}
+                                <span v-if="subitem.desc">{{
+                                    subitem.desc
+                                }}</span></a
+                            ></el-dropdown-item
+                        >
+                    </el-dropdown-menu>
+                </el-dropdown>
             </template>
             <template v-else>
                 <a
