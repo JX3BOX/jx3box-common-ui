1b818d92cf2fc429dbd83b0d7f2e1bb3f216f7a7
iRuxu <rx6@qq.com>
2021-04-05 15:52:31 +0800

    fix zlp

--- src/filters/zlpBy.vue
+++ src/filters/zlpBy.vue
@@ -2,53 +2,60 @@
     <span class="w-filter-menu">
         <el-dropdown>
             <span class="el-dropdown-link">
-                <span class="u-menu-label"
-                    ><i class="el-icon-s-operation"></i
-                    >{{ value ? value : '资料片' }}</span
-                ><i class="el-icon-arrow-down el-icon--right"></i>
+                <span class="u-menu-label">
+                    <i class="el-icon-s-operation"></i>
+                    {{ value ? value : '资料片' }}
+                </span>
+                <i class="el-icon-arrow-down el-icon--right"></i>
             </span>
             <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item @click.native="filter('')">{{
+                <el-dropdown-item @click.native="filter('')">
+                    {{
                     "全部"
-                }}</el-dropdown-item>
+                    }}
+                </el-dropdown-item>
                 <el-dropdown-item
                     v-for="item in data"
                     :key="item"
                     @click.native="filter(item)"
-                    >{{ item }}</el-dropdown-item
-                >
+                >{{ item }}</el-dropdown-item>
             </el-dropdown-menu>
         </el-dropdown>
     </span>
 </template>
 
 <script>
-import zlps from '@jx3box/jx3box-data/data/box/zlp.json'
+import zlps from "@jx3box/jx3box-data/data/box/zlp.json";
 export default {
     name: "zlpBy",
-    props: ['client'],
-    data: function() {
+    props: ["client"],
+    data: function () {
         return {
             value: "",
         };
     },
     computed: {
-        data : function (){
-            let client = this.client || 'all'
-            return zlps[client]
-        }
-    },
-    methods: {
-        filter: function(key) {
-            this.value = key;
-            this.$emit("filter", { type: 'zlp', val: key });
+        data: function () {
+            let client = this.client || "all";
+            return zlps[client];
         },
     },
-    mounted: function() {},
+    watch: {
+        client: function () {
+            this.value = "";
+        },
+    },
+    methods: {
+        filter: function (key) {
+            this.value = key;
+            this.$emit("filter", { type: "zlp", val: key });
+        },
+    },
+    mounted: function () {},
     components: {},
 };
 </script>
 
 <style lang="less">
-    @import '../../assets/css/menu-by.less';
+@import "../../assets/css/menu-by.less";
 </style>
