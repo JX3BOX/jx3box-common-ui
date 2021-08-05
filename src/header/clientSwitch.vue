7d8870d6d6f30663fd10378285820d481d916b2f
iRuxu <rx6@qq.com>
2021-08-05 20:16:59 +0800

    fix index

--- src/header/clientSwitch.vue
+++ src/header/clientSwitch.vue
@@ -52,7 +52,11 @@ export default {
             this.tempClient = this.defaultValue
         },
         go : function (item){
-            location.href = location.href.replace(item.from,item.to)
+            if(location.pathname.startsWith('/index') || location.pathname.startsWith('/origin')){
+                location.href = 'https://' + item.to
+            }else{
+                location.href = location.href.replace(item.from,item.to)
+            }
         },
     },
     mounted: function () {
