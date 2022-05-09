bffec76c15ae7ec29d176dca741d76970b3bf854
iRuxu <rx6@qq.com>
2022-05-09 17:27:44 +0800

    fix:medal size

--- src/Author.vue
+++ src/Author.vue
@@ -3,7 +3,7 @@
         <div class="u-author">
             <Avatar class="u-avatar" :uid="uid" :url="data.user_avatar" :size="68" :frame="data.user_avatar_frame" />
             <div class="u-info">
-                <a class="u-name" :href="authorLink(uid)">
+                <a class="u-name" :href="authorLink(uid)" target="_blank">
                     <span>{{ data.display_name.slice(0, 8) }}</span>
                     <el-tooltip class="item" effect="dark" content="签约作者" placement="top" v-if="isSuperAuthor">
                         <a class="u-superauthor" href="/dashboard/#/cooperation" target="_blank">
