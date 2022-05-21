dd95e6fedf20b26232e89276fbedf1dc28144b8a
Kaviilee <jiaweilee95@126.com>
2022-05-21 19:42:08 +0800

    perf: update medal

--- src/medal/medal.vue
+++ src/medal/medal.vue
@@ -1,41 +1,27 @@
 <template>
     <div class="m-medal">
-        <a :href="medalLink(item)" class="u-medal" v-for="item in medals" :key="item.id" :title="item.medal_desc">
-            <img class="u-medal-img" :onerror="`this.src=${errorPath}`" :src="medalImagePath(item.medal)" alt="">
+        <a :href="medalLink(item)" target="_blank" class="u-medal" v-for="item in medals" :key="item.id" :title="item.medal_desc">
+            <img class="u-medal-img" :src="showIcon(item.medal)" alt="">
         </a>
     </div>
 </template>
 
 <script>
-import { __imgPath } from '@jx3box/jx3box-common/data/jx3box.json'
 import { getMedalLink } from '@jx3box/jx3box-common/js/utils'
 export default {
     name: 'author_medal',
     props: {
         medals: {
             type: Array,
-            default: () => [], // [{ id, medal_desc, medal }]
+            default: () => [], // [{ rank_id, medal_desc, medal }]
         },
-        subtype: {
-            type: String,
-            default: '', // team, user
-        },
-    },
-    watch: {
-        authorId: {
-            immediate: true,
-            handler(val) {
-                val && this.loadAuthorMedal()
-            }
+        showIcon: {
+            type: Function,
+            default: () => true,
         }
     },
     methods: {
-        medalImagePath(medal) {
-            return `${__imgPath}image/medals/${this.subtype}/${medal}.gif`
-        },
-        errorPath(medal) {
-            return `${__imgPath}image/medals/${this.subtype}/${medal}.png`
-        },
+
         medalLink({ rank_id, subtype }) {
             return getMedalLink(rank_id, subtype)
         }
@@ -43,7 +29,7 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 .m-medal {
     display: block;
     margin: 5px 0;
