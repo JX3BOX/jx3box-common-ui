c56160d07ee88362251745018cc48c27104ceddc
iRuxu <rx6@qq.com>
2022-12-24 21:49:22 +0800

    feat:left sidebar & affix

--- src/single/cms-single.vue
+++ src/single/cms-single.vue
@@ -1,108 +1,69 @@
 <template>
-  <div class="m-single-box">
-    <!-- 头部 -->
-    <PostHeader
-      :post="post"
-      :stat="stat"
-    >
-      <slot name="single-header"></slot>
-    </PostHeader>
+    <div class="m-single-box">
+        <!-- 头部 -->
+        <PostHeader :post="post" :stat="stat">
+            <slot name="single-header"></slot>
+        </PostHeader>
 
-    <!-- 文章前 -->
-    <div class="m-single-prepend">
-      <!-- 联合创作者 -->
-      <Creators
-        class="m-single-creators"
-        :postId="id"
-        :postType="post_type"
-        @load-authors="loadAuthors"
-      />
-      <!-- 文集小册 -->
-      <Collection
-        class="m-single-collection"
-        :id="collection_id"
-        :defaultVisible="collection_collapse"
-        @collectionUpdate="updateCollection"
-      />
-      <slot name="single-prepend"></slot>
+        <!-- 文章前 -->
+        <div class="m-single-prepend">
+            <!-- 联合创作者 -->
+            <Creators class="m-single-creators" :postId="id" :postType="post_type" @load-authors="loadAuthors" />
+            <!-- 文集小册 -->
+            <Collection
+                class="m-single-collection"
+                :id="collection_id"
+                :defaultVisible="collection_collapse"
+                @collectionUpdate="updateCollection"
+            />
+            <slot name="single-prepend"></slot>
+        </div>
+
+        <!-- 文章内容 -->
+        <div class="m-single-post" v-if="visible">
+            <el-divider content-position="left">JX3BOX</el-divider>
+            <div class="m-single-content">
+                <slot></slot>
+                <ArticleMarkdown v-if="isMarkdown" :content="post_content" @directoryRendered="updateDirectory" />
+                <Article v-else :content="post_content" @directoryRendered="updateDirectory" />
+            </div>
+        </div>
+        <div class="m-single-null" v-else>
+            <el-alert :title="null_tip" type="warning" show-icon></el-alert>
+        </div>
+
+        <!-- 文章后 -->
+        <div class="m-single-append">
+            <slot name="single-append"></slot>
+
+            <!-- 打赏 -->
+            <Thx
+                class="m-single-thx"
+                :postId="id"
+                :postType="post_type"
+                :postTitle="post_title"
+                :userId="author_id"
+                :adminBoxcoinEnable="true"
+                :userBoxcoinEnable="true"
+                :authors="authors"
+                :client="post_client"
+            />
+
+            <!-- 评论 -->
+            <div class="m-single-comment">
+                <el-divider content-position="left">评论</el-divider>
+                <Comment :id="id" category="post" v-if="id && allow_comment" />
+                <el-alert title="作者没有开启评论功能" type="warning" show-icon v-else></el-alert>
+            </div>
+        </div>
+
+        <!-- 底部 -->
+        <footer class="m-single-footer">
+            <slot name="single-footer"></slot>
+        </footer>
+
+        <right-affix :postId="id" :postType="post_type" :postTitle="post_title"></right-affix>
     </div>
-
-    <!-- 文章内容 -->
-    <div
-      class="m-single-post"
-      v-if="visible"
-    >
-      <el-divider content-position="left">JX3BOX</el-divider>
-      <div class="m-single-content">
-        <slot></slot>
-        <ArticleMarkdown
-          v-if="isMarkdown"
-          :content="post_content"
-          @directoryRendered="updateDirectory"
-        />
-        <Article
-          v-else
-          :content="post_content"
-          @directoryRendered="updateDirectory"
-        />
-      </div>
-    </div>
-    <div
-      class="m-single-null"
-      v-else
-    >
-      <el-alert
-        :title="null_tip"
-        type="warning"
-        show-icon
-      ></el-alert>
-    </div>
-
-    <!-- 文章后 -->
-    <div class="m-single-append">
-      <slot name="single-append"></slot>
-
-      <!-- 打赏 -->
-      <Thx
-        class="m-single-thx"
-        :postId="id"
-        :postType="post_type"
-        :postTitle="post_title"
-        :userId="author_id"
-        :adminBoxcoinEnable="true"
-        :userBoxcoinEnable="true"
-        :authors="authors"
-        :client="post_client"
-      />
-
-      <!-- 评论 -->
-      <div class="m-single-comment">
-        <el-divider content-position="left">评论</el-divider>
-        <Comment
-          :id="id"
-          category="post"
-          v-if="id && allow_comment"
-        />
-        <el-alert
-          title="作者没有开启评论功能"
-          type="warning"
-          show-icon
-          v-else
-        ></el-alert>
-      </div>
-    </div>
-
-    <!-- 底部 -->
-    <footer class="m-single-footer">
-      <slot name="single-footer"></slot>
-    </footer>
-
-    <right-affix
-      :postId="id"
-      :postType="post_type"
-      :postTitle="post_title"
-    ></right-affix>
-  </div>
 </template>
 
 <script>
@@ -117,121 +78,121 @@ import Comment from "@jx3box/jx3box-comment-ui/src/Comment.vue";
 import { __visibleMap } from "@jx3box/jx3box-common/data/jx3box.json";
 import { getAppType } from "@jx3box/jx3box-common/js/utils";
 export default {
-  name: "cms-single",
-  components: {
-    PostHeader,
-    Creators,
-    Collection,
-    Thx,
-    Article,
-    ArticleMarkdown,
-    Comment,
-    RightAffix
-  },
-  props: ["post", "stat"],
-  data: function () {
-    return {
-      collection_data: "",
-      directory: false,
+    name: "cms-single",
+    components: {
+        PostHeader,
+        Creators,
+        Collection,
+        Thx,
+        Article,
+        ArticleMarkdown,
+        Comment,
+        RightAffix,
+    },
+    props: ["post", "stat"],
+    data: function () {
+        return {
+            collection_data: "",
+            directory: false,
 
-      // 创作者
-      other_authors: [],
-      super_author: "",
-    };
-  },
-  computed: {
-    id: function () {
-      return ~~this.post?.ID || 0;
+            // 创作者
+            other_authors: [],
+            super_author: "",
+        };
     },
-    app_type: function () {
-      return getAppType();
+    computed: {
+        id: function () {
+            return ~~this.post?.ID || 0;
+        },
+        app_type: function () {
+            return getAppType();
+        },
+        post_type: function () {
+            return this.post?.post_type;
+        },
+        post_title: function () {
+            return this.post?.post_title;
+        },
+        author_id: function () {
+            return this.post?.post_author;
+        },
+        collection_id: function () {
+            return this.post?.post_collection;
+        },
+        collection_collapse: function () {
+            return this.post?.collection_collapse;
+        },
+        visible: function () {
+            return !!this.post?._check;
+        },
+        null_tip: function () {
+            let str = "作者设置了【";
+            str += __visibleMap[this.post?.visible];
+            str += "】";
+            return str;
+        },
+        post_content: function () {
+            return this.post?.post_content || "";
+        },
+        post_mode: function () {
+            return this.post?.post_mode || "tinymce";
+        },
+        isMarkdown: function () {
+            return this.post_mode == "markdown";
+        },
+        allow_comment: function () {
+            return !this.post?.comment;
+        },
+        extend_data: function () {
+            return {
+                collection_data: this.collection_data,
+                directory: this.directory,
+            };
+        },
+        authors: function () {
+            if (this.other_authors && this.other_authors.length) {
+                return [this.super_author, ...this.other_authors];
+            }
+            return [];
+        },
+        post_client: function () {
+            return this.post?.client || "all";
+        },
     },
-    post_type: function () {
-      return this.post?.post_type;
+    methods: {
+        updateCollection: function (val) {
+            this.collection_data = val;
+        },
+        updateDirectory: function (val) {
+            this.directory = val;
+        },
+        loadAuthors({ super_author, other_authors }) {
+            this.super_author = super_author;
+            this.other_authors = other_authors;
+        },
     },
-    post_title: function () {
-      return this.post?.post_title;
-    },
-    author_id: function () {
-      return this.post?.post_author;
-    },
-    collection_id: function () {
-      return this.post?.post_collection;
-    },
-    collection_collapse: function () {
-      return this.post?.collection_collapse;
-    },
-    visible: function () {
-      return !!this.post?._check;
-    },
-    null_tip: function () {
-      let str = "作者设置了【";
-      str += __visibleMap[this.post?.visible];
-      str += "】";
-      return str;
-    },
-    post_content: function () {
-      return this.post?.post_content || "";
-    },
-    post_mode: function () {
-      return this.post?.post_mode || "tinymce";
-    },
-    isMarkdown: function () {
-      return this.post_mode == "markdown";
-    },
-    allow_comment: function () {
-      return !this.post?.comment;
-    },
-    extend_data: function () {
-      return {
-        collection_data: this.collection_data,
-        directory: this.directory,
-      };
-    },
-    authors: function () {
-      if (this.other_authors && this.other_authors.length) {
-        return [this.super_author, ...this.other_authors];
-      }
-      return [];
-    },
-    post_client: function () {
-      return this.post?.client || "all";
-    },
-  },
-  methods: {
-    updateCollection: function (val) {
-      this.collection_data = val;
-    },
-    updateDirectory: function (val) {
-      this.directory = val;
-    },
-    loadAuthors ({ super_author, other_authors }) {
-      this.super_author = super_author;
-      this.other_authors = other_authors;
-    },
-  },
-  watch: {
-    extend_data: {
-      deep: true,
-      immediate: true,
-      handler: function (val) {
-        this.$emit("extendUpdate", val);
-      },
-    },
-    post_type: {
-      deep: true,
-      immediate: true,
-      handler: function (val) {
-        if (location.host.includes("localhost")) {
-          return;
-        }
+    watch: {
+        extend_data: {
+            deep: true,
+            immediate: true,
+            handler: function (val) {
+                this.$emit("extendUpdate", val);
+            },
+        },
+        post_type: {
+            deep: true,
+            immediate: true,
+            handler: function (val) {
+                if (location.host.includes("localhost")) {
+                    return;
+                }
 
-        if (val && val !== this.app_type) {
-          location.href = `/${val}/${this.id}`;
-        }
-      },
+                if (val && val !== this.app_type) {
+                    location.href = `/${val}/${this.id}`;
+                }
+            },
+        },
     },
-  },
 };
 </script>
 
--- src/single/right-affix.vue
+++ src/single/right-affix.vue
@@ -1,85 +1,68 @@
 <template>
-  <div class="right-affix">
-    <div class="item">
-      <fav
-        :postId="postId"
-        :postType="postType"
-        :postTitle="postTitle"
-        :hiddenNum="true"
-      ></fav>
+    <div class="right-affix">
+        <div class="item">
+            <fav :postId="postId" :postType="postType" :postTitle="postTitle" :hiddenNum="true"></fav>
+        </div>
+        <el-tooltip effect="dark" content="回到顶部" placement="bottom">
+            <div class="item" v-show="scrollBtnShow" @click="goTop">
+                <div class="to-top"></div>
+            </div>
+        </el-tooltip>
     </div>
-    <el-tooltip
-      effect="dark"
-      content="回到顶部"
-      placement="bottom"
-    >
-      <div
-        class="item"
-        v-show="scrollBtnShow"
-        @click="goTop"
-      >
-        <div class="to-top"></div>
-      </div>
-    </el-tooltip>
-  </div>
 </template>
 
 <script>
 import Fav from "../interact/Fav2.vue";
 export default {
-  name: 'RightAffix',
-  props: [
-    "postId",
-    "postType",
-    "postTitle",
-  ],
-  data () {
-    return {
-      scrollToptimer: null,
-      scrollBtnShow: false,
-      isTop: true
-    }
-  },
-  components: {
-    Fav,
-  },
-  methods: {
-    goTop () {
-      //设置定时器
-      const self = this
-      this.scrollToptimer = setInterval(function () {
-        //获取滚动条距离顶部高度
-        var osTop = document.documentElement.scrollTop || document.body.scrollTop;
-        var ispeed = Math.floor(-osTop / 7);
-        document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
-        //到达顶部，清除定时器
-        if (osTop === 0) {
-          clearInterval(self.scrollToptimer);
+    name: "RightAffix",
+    props: ["postId", "postType", "postTitle"],
+    data() {
+        return {
+            scrollToptimer: null,
+            scrollBtnShow: false,
+            isTop: true,
         };
-        self.isTop = true;
-      }, 30);
-    }
-  },
-  mounted () {
-    //获取页面可视区高度
-    var clientHeight = document.documentElement.clientHeight;
-    const self = this
-    window.addEventListener('scroll', function () {
-      //显示回到顶部按钮
-      var osTop = document.documentElement.scrollTop || document.body.scrollTop;
-      if (osTop >= clientHeight) {
-        self.scrollBtnShow = true
-      } else {
-        self.scrollBtnShow = false
-      };
-      //回到顶部过程中用户滚动滚动条，停止定时器
-      if (!self.isTop) {
-        clearInterval(self.scrollToptimer);
-      };
-      self.isTop = false;
-    })
-  }
-}
+    },
+    components: {
+        Fav,
+    },
+    methods: {
+        goTop() {
+            //设置定时器
+            const self = this;
+            this.scrollToptimer = setInterval(function () {
+                //获取滚动条距离顶部高度
+                var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+                var ispeed = Math.floor(-osTop / 7);
+                document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
+                //到达顶部，清除定时器
+                if (osTop === 0) {
+                    clearInterval(self.scrollToptimer);
+                }
+                self.isTop = true;
+            }, 30);
+        },
+    },
+    mounted() {
+        //获取页面可视区高度
+        var clientHeight = document.documentElement.clientHeight;
+        const self = this;
+        window.addEventListener("scroll", function () {
+            //显示回到顶部按钮
+            var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+            if (osTop >= clientHeight) {
+                self.scrollBtnShow = true;
+            } else {
+                self.scrollBtnShow = false;
+            }
+            //回到顶部过程中用户滚动滚动条，停止定时器
+            if (!self.isTop) {
+                clearInterval(self.scrollToptimer);
+            }
+            self.isTop = false;
+        });
+    },
+};
 </script>
 
 <style lang="less" scoped>
@@ -89,28 +72,35 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    right: 20px;
-    top: 25%;
+    right: @aside-right;
+    top: @header-height + @bread-height + 100px;
     background-color: #fafbfc;
-    padding: 5px;
+    padding: 8px 5px;
     z-index: 99999;
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
     .item {
         display: flex;
         justify-content: center;
         align-items: center;
-        width: 35px;
-        height: 35px;
+        width: 32px;
+        height: 32px;
         cursor: pointer;
         &:hover {
-            background-color: #eee;
+            background-color: #f6fcff;
         }
     }
     .to-top {
         width: 0;
         height: 0;
-        border-left: 10px solid transparent;
-        border-right: 10px solid transparent;
-        border-bottom: 15px solid skyblue;
+        border-left: 12px solid transparent;
+        border-right: 12px solid transparent;
+        border-bottom: 18px solid skyblue;
+    }
+}
+@media screen and (max-width:@smallpc){
+    .right-affix{
+        right:0;
     }
 }
 </style>
