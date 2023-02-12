a14aace40ec46bcacd681a5cb8978c1638e85b73
iRuxu <rx6@qq.com>
2023-02-13 05:10:35 +0800

    perf:filters

--- src/filters/markBy.vue
+++ src/filters/markBy.vue
@@ -1,9 +1,9 @@
 <template>
-    <span class="w-filter-menu" v-if="mode == 'menu'">
+    <span class="w-filter-menu">
         <el-dropdown>
             <span class="el-dropdown-link">
                 <span class="u-menu-label"
-                    ><i class="el-icon-medal-1"></i
+                    ><i class="el-icon-s-operation"></i
                     >{{ current ? current : deftext }}</span
                 ><i class="el-icon-arrow-down el-icon--right"></i>
             </span>
@@ -20,7 +20,7 @@
             </el-dropdown-menu>
         </el-dropdown>
     </span>
-    <div class="w-filter-mark" :class="{ on: visible }" v-else>
+    <!-- <div class="w-filter-mark" :class="{ on: visible }" v-else>
         <span class="u-label" @click="toggleFilter">
             <span class="u-current-filter">筛选 : {{ current || "全部" }}</span>
             <span class="u-toggle">
@@ -45,7 +45,7 @@
             >
             <slot></slot>
         </span>
-    </div>
+    </div> -->
 </template>
 
 <script>
@@ -70,7 +70,7 @@ export default {
             return marks[this.mark];
         },
         deftext: function() {
-            return this.placeholder || "难度";
+            return this.placeholder || "精选";
         },
     },
     methods: {
@@ -89,5 +89,102 @@ export default {
 </script>
 
 <style lang="less">
-    @import '../../assets/css/mark-by.less';
+.w-filter-menu {
+    .pointer;
+    .u-menu-label {
+        i {
+            .mr(5px);
+        }
+        .fz(12px);
+    }
+}
+.w-filter-mark {
+    .fz(12px);
+
+    .u-label {
+        .none;
+    }
+    .u-toggle {
+        .pa;
+        .rt(0);
+        padding: 6px 10px;
+        .pointer;
+
+        .el-icon-arrow-up {
+            .none;
+        }
+
+        &.on {
+            .el-icon-arrow-down {
+                .none;
+            }
+            .el-icon-arrow-up {
+                .db;
+            }
+        }
+    }
+    .u-mode {
+        .mr(15px);
+        .pointer;
+        i {
+            .mr(2px);
+        }
+        &.on {
+            color: @primary;
+        }
+        &:hover {
+            color: @pink;
+        }
+    }
+}
+@media screen and (max-width: @ipad) {
+    .w-filter-mark {
+        .pr;
+        background-color: #f6f8fa;
+        border: 1px solid #eee;
+        .r(4px);
+        user-select: none;
+        padding: 5px 0;
+        padding-right: 20px;
+        min-width: 110px;
+        box-sizing: border-box;
+
+        &.on {
+            .u-options {
+                .db;
+            }
+
+            border-bottom-left-radius: 0;
+            border-bottom-right-radius: 0;
+        }
+
+        .u-label {
+            padding: 0 10px;
+            .pointer;
+            .db;
+        }
+        .u-options {
+            .pa;
+            .lt(-1px, 29px);
+            padding: 5px 0;
+            .w(100%);
+            background-color: #fff;
+            border: 1px solid #ddd;
+            .z(2);
+            .u-mode {
+                .db;
+                padding: 7px 10px;
+                .pr;
+                .ml(0);
+            }
+            .none;
+        }
+    }
+}
+@media screen and (max-width: @phone) {
+    .w-filter-mark {
+        .none;
+    }
+}
+
 </style>
