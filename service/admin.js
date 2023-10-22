00799cd004e99558922cea581369bfd416a302e6
fifthThirteen <570205219@qq.com>
2023-10-23 00:11:31 +0800

    fix:文章管理接口更新

--- service/admin.js
+++ service/admin.js
@@ -2,33 +2,33 @@ import { $cms } from "@jx3box/jx3box-common/js/https.js";
 
 function getSetting(id) {
     return $cms()
-        .get(`/api/cms/post/${id}/query`)
+        .get(`/api/cms/manage/post/${id}`)
         .then((res) => {
             return res.data.data;
         });
 }
 
 function postSetting(data) {
-    return $cms().put(`/api/cms/post/${data.ID}/setting`, data);
+    return $cms().put(`/api/cms/manage/post/${data.ID}`, data);
 }
 
 // 管理员发送私信
-function sendMessage(data){
+function sendMessage(data) {
     return $cms().post(`/api/cms/admin/direct-message`, data);
 }
 
 // 管理员新增自定义主题
-function addTopicBucket(data){
+function addTopicBucket(data) {
     return $cms().post(`/api/cms/post/topic/bucket`, data);
 }
 
 // 管理员删除自定义主题
-function deleteTopicBucket(id){
+function deleteTopicBucket(id) {
     return $cms().delete(`/api/cms/post/topic/bucket/${id}`);
 }
 
 // 管理员修改自定义主题
-function updateTopicBucket(id, data){
+function updateTopicBucket(id, data) {
     return $cms().put(`/api/cms/post/topic/bucket/${id}`, data);
 }
 
